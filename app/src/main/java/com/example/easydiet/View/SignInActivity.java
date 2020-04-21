3a8e5a6ce326e61d8cec5b27cde68a89e01147f8
Arye Amsalem <arye.amsalem@gmail.com>
2020-04-21 15:03:30 +0300

    update android x issues

--- app/src/main/java/com/example/easydiet/View/SignInActivity.java
+++ app/src/main/java/com/example/easydiet/View/SignInActivity.java
@@ -1,14 +1,47 @@
 package com.example.easydiet.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.example.easydiet.R;
 
 
 public class SignInActivity extends AppCompatActivity {
 
+    private static final String TAG = "MyMessage";
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    EditText age;
+    EditText gender;
+    EditText weight;
+    EditText height;
+    Button saveContinue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        // initialize
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.gender);
+        weight = findViewById(R.id.weight);
+        height = findViewById(R.id.heigth);
+        saveContinue = findViewById(R.id.button_saveContinue);
+
+        saveContinue.setOnClickListener(new View.OnClickListener(){
+          @Override
+          public void onClick(View view){
+              // TODO Save to database
+              Log.i(TAG, "Saving New User: firstName: " + firstName.getText().toString());
+          }
+        });
+
     }
 }
